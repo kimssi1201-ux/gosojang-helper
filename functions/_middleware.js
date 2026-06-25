0a1500956526f39499f730d0af1ff4a6cc8e689d
kimssi1201-ux <kimssi1201@gmail.com>
2026-06-26 02:29:23 +0900

    Allow cross-origin API responses

---
+++ functions/_middleware.js
@@ -0,0 +1,23 @@
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
+export async function onRequest(context) {
+  if (context.request.method === "OPTIONS") {
+    return new Response(null, { status: 204, headers: corsHeaders });
+  }
+
+  const response = await context.next();
+  const headers = new Headers(response.headers);
+  for (const [key, value] of Object.entries(corsHeaders)) {
+    headers.set(key, value);
+  }
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers,
+  });
+}
